4bdb1a4b8a14014c4018373236bb006927dc7955
Cléber Severo <clebersevero1998@gmail.com>
2024-12-08 14:23:25 -0300

    feat: test configs
    test package json;
    test providers wrapper;

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,5 +1,15 @@
+import { Provider } from "react-redux";
+import Home from ".";
+import store from "../../store";
+import { render } from "test-utils";
+
 describe("Testando a página Home", () => {
   test("Testando", () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
     expect(true).toBeTruthy();
   });
 });
