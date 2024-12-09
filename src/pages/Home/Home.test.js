5fb54b5d3c46c83758c961f15cf711937d745dbf
Cléber Severo <clebersevero1998@gmail.com>
2024-12-08 23:39:26 -0300

    feat: axios mock testing

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,15 +1,14 @@
-import { Provider } from "react-redux";
 import Home from ".";
-import store from "../../store";
-import { render } from "test-utils";
+import { render, screen } from "test-utils";
+
+jest.mock("../../services/categorias");
 
 describe("Testando a página Home", () => {
-  test("Testando", () => {
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-    expect(true).toBeTruthy();
+  test("deve renderizar a página Home com categorias", async () => {
+    render(<Home />);
+
+    const categorias = await screen.findAllByTestId("home-categorias");
+
+    expect(categorias).toHaveLength(2);
   });
 });
